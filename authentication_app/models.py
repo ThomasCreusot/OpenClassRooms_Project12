a04166d799948008a921578b063e2558be6122ad
Thomas Creusot <th.creusot@gmail.com>
2022-11-21 11:59:35 +0100

    user model created

--- authentication_app/models.py
+++ authentication_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# OC classes: even if the User model by default is appropriate, we advice to # always implement a
+# personalizerd User model (even if identical to defaul model)
+class User(AbstractUser):
+    """Reprensents an User"""
+
+    MANAGEMENT = 'MANAGEMENT'
+    SALES = 'SALES'
+    SUPPORT = 'SUPPORT'
+
+    TEAM_CHOICES = (
+        (MANAGEMENT, 'Management team'),
+        (SALES, 'Sales team'),
+        (SUPPORT, 'Support team'),
+    )
+
+    # user_id = models.fields.IntegerField   # already in the AbstractUser model
+    # first_name = models.fields.CharField  # already in the AbstractUser model
+    # last_name = models.fields.CharField  # already in the AbstractUser model
+    # email = models.fields.EmailField   # already in the AbstractUser model
+    # password = models.fields.CharField   # already in the AbstractUser model
+    team = models.fields.CharField(max_length=255, choices=TEAM_CHOICES, blank=False)
