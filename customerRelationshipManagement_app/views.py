4d506394c727d173cdd0a5b7e9973c5928074826
Thomas Creusot <th.creusot@gmail.com>
2022-11-22 10:12:50 +0100

    Creation of ClientViewset in views.py in customerRelationshipManagement_app

--- customerRelationshipManagement_app/views.py
+++ customerRelationshipManagement_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
-from customerRelationshipManagement_app.models import Client
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from customerRelationshipManagement_app.models import Client
+from customerRelationshipManagement_app.serializers import ClientSerializer
+
+
 def index(request):
     obj = Client.objects.all()
     context={
         "obj": obj,
     }
     return render(request, 'index.html', context=context)
+
+
+class ClientViewset(ModelViewSet):
+    """API endpoint that allows Clients to be CRUD."""
+
+    serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        # return Client.objects.filter(=self.kwargs[''])
+        return Client.objects.all()
