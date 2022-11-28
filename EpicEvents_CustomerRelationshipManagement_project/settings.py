a02b3cbb83a7f8bd30ec71419d9433ae64bb2e92
Thomas Creusot <th.creusot@gmail.com>
2022-11-28 14:52:19 +0100

    JWT installation and update of requirements.txt settings.py and urls.py

--- EpicEvents_CustomerRelationshipManagement_project/settings.py
+++ EpicEvents_CustomerRelationshipManagement_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'authentication_app',
     'customerRelationshipManagement_app',
 ]
@@ -131,3 +133,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'authentication_app.User'
+
+LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/api/clients/'
+
+# Authentication class from JWT
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
--- EpicEvents_CustomerRelationshipManagement_project/urls.py
+++ EpicEvents_CustomerRelationshipManagement_project/urls.py
@@ -20,6 +20,7 @@ from rest_framework import routers
  
 from customerRelationshipManagement_app.views import index, ClientViewset, ContractViewset, EventViewset
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.SimpleRouter()
 router.register('clients', ClientViewset, basename='clients') 
@@ -29,6 +30,9 @@ router.register('events', EventViewset, basename='events')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('api/', include(router.urls)),
 
